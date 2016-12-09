6e9b5da14771c8845a324cb471212f28ae0bc565
Eric Tucker <Tucker.Ericm@gmail.com>
2016-12-09 13:33:26 -0800

    update to allow url params to prepop the form

--- js/app.js
+++ js/app.js
@@ -1,41 +1,50 @@
-var vm = new Vue({
-  el: '#emailsig',
-  data: {
-    name: '',
-    phone: '',
-    email: '',
-    nmls: '',
-    title: '',
-    fax: '',
-    htmlCode: document.getElementById('signature').innerHTML.replace(/^\s*\n/gm, '').replace(/\t/g,"")
-  },
-  watch: {
-    'name': 'compileRawHtml',
-    'phone': 'compileRawHtml',
-    'email': 'compileRawHtml',
-    'nmls': 'compileRawHtml',
-    'title': 'compileRawHtml',
-    'fax': 'compileRawHtml'
-  },  
-  methods: {
-    compileRawHtml() {
-      this.htmlCode = this.$interpolate(document.getElementById('signature').innerHTML.replace(/^\s*\n/gm, '').replace(/\t/g,""));
+const vm = new Vue({
+    el: '#emailsig',
+    attached: function () {
+        window.location.search
+            .replace(/^\?/, '')
+            .split('&')
+            .forEach(pair => {
+                let [param, val = ''] = decodeURIComponent(pair).split('=');
+                this[param] = val;
+            });
     },
-  	selectCode() {
-  		var text = document.getElementById('displayedHtml');
-		  var selection = window.getSelection();
-      var range = document.createRange();
-      range.selectNodeContents(text);
-      selection.removeAllRanges();
-      selection.addRange(range);
-  	},
-  	selectSignature() {
-  		var text = document.getElementById('signature');
-		  var selection = window.getSelection();
-      var range = document.createRange(); 
-      range.selectNodeContents(text);
-      selection.removeAllRanges();
-      selection.addRange(range);
-  	}
-  }
+    data: {
+        name: '',
+        phone: '',
+        email: '',
+        nmls: '',
+        title: '',
+        fax: '',
+        htmlCode: ''
+    },
+    watch: {
+        'name': 'compileRawHtml',
+        'phone': 'compileRawHtml',
+        'email': 'compileRawHtml',
+        'nmls': 'compileRawHtml',
+        'title': 'compileRawHtml',
+        'fax': 'compileRawHtml'
+    },
+    methods: {
+        compileRawHtml() {
+            this.htmlCode = this.$interpolate(document.getElementById('signature').innerHTML).replace(/>\s+<([^\/])/g, '>\n<$1');
+        },
+        selectCode() {
+            const text = document.getElementById('displayedHtml');
+            const selection = window.getSelection();
+            const range = document.createRange();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        },
+        selectSignature() {
+            const text = document.getElementById('signature');
+            const selection = window.getSelection();
+            const range = document.createRange();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
 });
